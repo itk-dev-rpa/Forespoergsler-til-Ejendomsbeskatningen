de83cf6a185c9a336a64e5b65453e5709812bb0d
kri-bak <kriba@aarhus.dk>
2025-03-14 09:42:02 +0100

    Removed unused code

--- robot_framework/sub_process/go_process.py
+++ robot_framework/sub_process/go_process.py
@@ -26,6 +26,28 @@ def create_session(username: str, password: str) -> Session:
     return session
 
 
+def create_case(session: Session, apiurl: str, title: str, case_type: str = Literal["EMN", "GEO"]) -> tuple[str, Session]:
+    """Create a case in GetOrganized.
+
+    Args:
+        apiurl: Url for the GetOrganized API.
+        session: Session object to access API.
+        title: Title of the case being created.
+
+    Returns:
+        Return the response and session objects.
+    """
+    url = urljoin(apiurl, "/_goapi/Cases/")
+    payload = {
+        'CaseTypePrefix': case_type,
+        'MetadataXml': f'<z:row xmlns:z="#RowsetSchema" ows_Title="{title}" ows_CaseStatus="Åben" ows_CaseCategory="Åben for alle" ows_Afdeling="916;#Backoffice - Drift og Økonomi" ows_KLENummer="318;#25.02.00 Ejendomsbeskatning i almindelighed"/>',
+        'ReturnWhenCaseFullyCreated': False
+    }
+    response = session.post(url, data=json.dumps(payload), timeout=config.GO_TIMEOUT)
+    response.raise_for_status()
+    return response.text, session
+
+
 def upload_document(*, apiurl: str, file: bytearray, case: str, filename: str, agent_name: str | None = None, date_string: str | None = None, session: Session, doc_category: str | None = None) -> tuple[str, Session]:
     """Upload a document to Get Organized.
 
@@ -53,82 +75,5 @@ def upload_document(*, apiurl: str, file: bytearray, case: str, filename: str, a
         "Overwrite": True
     }
     response = session.post(url, data=json.dumps(payload), timeout=config.GO_TIMEOUT)
-    response.raise_for_status()
-    return response.text, session
-
-
-def delete_document(apiurl: str, document_id: int, session: Session) -> tuple[str, Session]:
-    """Delete a document from GetOrganized.
-
-    Args:
-        apiurl: Url of the GetOrganized API.
-        session: Session object used for logging in.
-        document_id: ID of the document to delete.
-
-    Returns:
-        Return the response and session objects
-    """
-    url = urljoin(apiurl, "/_goapi/Documents/ByDocumentId")
-    payload = {
-        "DocId": document_id
-    }
-    response = session.delete(url, data=json.dumps(payload), timeout=config.GO_TIMEOUT)
-    response.raise_for_status()
-    return response.text, session
-
-
-def create_case(session: Session, apiurl: str, title: str, case_type: str = Literal["EMN", "GEO"]) -> tuple[str, Session]:
-    """Create a case in GetOrganized.
-
-    Args:
-        apiurl: Url for the GetOrganized API.
-        session: Session object to access API.
-        title: Title of the case being created.
-
-    Returns:
-        Return the response and session objects.
-    """
-    url = urljoin(apiurl, "/_goapi/Cases/")
-    payload = {
-        'CaseTypePrefix': case_type,
-        'MetadataXml': f'<z:row xmlns:z="#RowsetSchema" ows_Title="{title}" ows_CaseStatus="Åben" ows_CaseCategory="Åben for alle" ows_Afdeling="916;#Backoffice - Drift og Økonomi" ows_KLENummer="318;#25.02.00 Ejendomsbeskatning i almindelighed"/>',
-        'ReturnWhenCaseFullyCreated': False
-    }
-    response = session.post(url, data=json.dumps(payload), timeout=config.GO_TIMEOUT)
-    response.raise_for_status()
-    return response.text, session
-
-
-def case_metadata(session: Session, apiurl: str, case_id: str):
-    """Get metadata for a GetOrganized case, to look through parameters and values.
-
-    Args:
-        session: Session token.
-        apiurl: Base URL for the API.
-        case_id: Case ID to get metadata on.
-
-    Returns:
-        Return the response and session objects.
-    """
-    url = urljoin(apiurl, f"/_goapi/Cases/Metadata/{case_id}")
-    response = session.get(url, timeout=config.GO_TIMEOUT)
-    response.raise_for_status()
-    return response.text, session
-
-
-def close_case(apiurl: str, case_number: str, session: Session) -> tuple[str, Session]:
-    """Close a case in GetOrganized.
-
-    Args:
-        apiurl: Url for the GetOrganized API.
-        session: Session object to access API.
-        case_number: Case number of case to be closed.
-
-    Returns:
-        Return the response and session objects.
-    """
-    url = urljoin(apiurl, "/_goapi/Cases/CloseCase")
-    payload = {"CaseId": case_number}
-    response = session.post(url, data=payload, timeout=config.GO_TIMEOUT)
     response.raise_for_status()
     return response.text, session
